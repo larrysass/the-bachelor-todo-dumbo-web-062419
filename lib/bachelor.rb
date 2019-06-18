87de71073c7abe003d213945100b2d7cc9a6537c
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-06-17 22:49:16 -0400

    bachelor

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+
+    data[season].each do |people|
+
+       people.each do |key ,value|
+        if value == "Winner"
+            full_name = people["name"].split(' ')
+            return full_name[0]
+        end
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+   array.each do |people|
+    people.each do |key, value|
+      if value == occupation
+        return people["name"]
+      end
+   end
+  end
+end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+counter = 0
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |key, value|
+        if value == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |key, value|
+        if value == hometown
+          return people["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  age_total = 0
+  number_of_people = 0
+    data[season].each do |people|
+      people.each do |key, value|
+      if key == "age"
+        number_of_people += 1
+        age_total += value.to_f
+      end
+    end
+  end
+average = (age_total / number_of_people).round
 end
